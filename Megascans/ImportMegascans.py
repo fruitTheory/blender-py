d37b98a4bddb31f70b3aa2bd839dbe2894a7686a
Derwin <derwinprince61@gmail.com>
2024-10-04 01:32:00 -0400

    Update texture import

--- Megascans/ImportMegascans.py
+++ Megascans/ImportMegascans.py
@@ -3,7 +3,6 @@ from bpy_extras.io_utils import ImportHelper
 from bpy.types import Operator
 import os
 import time
-time_start = time.time()
 
 print("Hello")
 
@@ -15,9 +14,124 @@ materials = data.materials
 images = data.images
 
 def SetupTextures(directory):
+
+    mtl_name = directory.rsplit("\\", 2)[-2]
     files = os.listdir(directory)
-    print("Directory:", directory)
-    print("Files in directory:", files)
+    
+    try:
+        # Check if relevant material exists
+        materials[mtl_name]
+    except:
+        # Material doesnt exists
+        newMat = materials.new(mtl_name)
+
+    else:
+        # Material does exists
+        print(f"Material: {mtl_name} already exists.")
+        return
+
+    # newMat.name = mtl_name
+    newMat.use_nodes = True
+    newMat.displacement_method = "BOTH"
+
+    # Get base node tree and node links
+    nodes = newMat.node_tree.nodes
+    nodes.clear()
+    node_links = newMat.node_tree.links
+    
+    # Containers for file types and full names
+    file_type = []
+    file_full = []
+    
+    # Filter for type of file - "Albedo, Normal, etc."
+    for file in files:
+        if file.endswith(".jpg") or file.endswith(".exr"):
+            file_full.append(file)
+            file = file.rsplit("_", 1)[-1]
+            file = file.split(".")[0]
+            file_type.append(file)
+    
+    # Containers for image node objects and dict for easier access
+    image_nodes = []
+    image_dictionary = {}
+    
+    # Creating the image nodes based on textures found
+    for x in range(len(file_type)):
+        image_node = nodes.new(type="ShaderNodeTexImage")
+        image_nodes.append(image_node)
+        image_dictionary[file_type[x].lower()] = image_node
+        
+        image_node.name = file_type[x]
+        image_node.location = ((-300 * x), 300)
+        image_node.image = images.load(filepath=directory+file_full[x], check_existing=True)
+        
+        # Color management for non-colored images
+        if file_type[x] not in ("Albedo", "Displacement", "Specular", "Translucency"):
+            image_node.image.colorspace_settings.name = "Non-Color"
+        
+        # In case of AO map
+        if file_type[x].lower() in ("ao"):
+            multiply_node = nodes.new(type="ShaderNodeMix")
+            multiply_node.data_type = "RGBA"
+            multiply_node.blend_type = "MULTIPLY"
+            multiply_node.clamp_result = True
+            multiply_node.location = (0, -300)
+            multiply_node.inputs[0].default_value = 1
+    
+    # Create rest of nodes
+    output_node = nodes.new(type="ShaderNodeOutputMaterial")
+    output_node.location = (600, 0)
+    bsdf_node = nodes.new(type="ShaderNodeBsdfPrincipled")
+    bsdf_node.location = (300, 0)
+
+    # Start making connections
+    node_links.new(bsdf_node.outputs["BSDF"], output_node.inputs["Surface"])
+
+    # If displacement create and link displacement to disp node and output
+    if "displacement" in image_dictionary.keys():
+        disp_node = nodes.new(type="ShaderNodeDisplacement")
+        disp_node.location = (-300, -300)
+        node_links.new(disp_node.outputs["Displacement"], output_node.inputs["Displacement"])
+        node_links.new(image_dictionary["displacement"].outputs["Color"], disp_node.inputs["Height"])
+
+    # Check if theres an ao node to multiply with albedo
+    if "ao" in image_dictionary.keys():
+        node_links.new(image_dictionary["albedo"].outputs["Color"], multiply_node.inputs["A"])
+        node_links.new(image_dictionary["ao"].outputs["Color"], multiply_node.inputs["B"])
+        node_links.new(multiply_node.outputs["Result"], bsdf_node.inputs["Base Color"])
+    else:
+        node_links.new(image_dictionary["albedo"].outputs["Color"], bsdf_node.inputs["Base Color"])
+        
+    # If normal create and Link normal to normalmap node and to BSDF  
+    if "normal" in image_dictionary.keys():
+        normal_node = nodes.new(type="ShaderNodeNormalMap")
+        normal_node.location = (-600, -300)
+        node_links.new(image_dictionary["normal"].outputs["Color"], normal_node.inputs["Color"])
+        node_links.new(normal_node.outputs["Normal"], bsdf_node.inputs["Normal"])
+
+    check_link(node_links, image_dictionary, "specular", bsdf_node, "Specular Tint")
+    check_link(node_links, image_dictionary, "roughness", bsdf_node, "Roughness")
+    check_link(node_links, image_dictionary, "opacity", bsdf_node, "Alpha")
+
+    # If normtranslucency create additional BSDF and additional linkage
+    if "translucency" in image_dictionary.keys():
+        # transparent_bsdf = nodes.new(type="ShaderNodeNormalMap")
+        # translucent_bsdf = nodes.new(type="ShaderNodeNormalMap")
+        # bsdf_mix_trans = nodes.new(type="ShaderNodeMix")
+        # bsdf_mix_main = nodes.new(type="ShaderNodeMix")
+        # Input opacity into translucent bsdf color and bsdf mix trans
+        # And transparent, translucent bsdf's
+        # input that mix trans into mix main
+        # and main mix will go into surface output
+        pass
+
+
+# Check if link can be made, needs nodelink obect, output node, its name, and input node and slot
+def check_link(node_links, node_out, node_name, node_in, in_type):
+    try:
+        node_links.new(node_out[node_name].outputs["Color"], node_in.inputs[in_type])
+    except:
+        pass
 
 
 # This class is a custom directory operator
@@ -34,12 +148,13 @@ class ImportTextures(Operator):
         return {'RUNNING_MODAL'}
 
     def execute(self, context):
+        time_start = time.time()
         SetupTextures(self.directory)
+        print("My Script Finished: %.4f sec" % (time.time() - time_start))
         return {'FINISHED'}
     
-    def run():
-        # Register and call
-        bpy.utils.register_class(ImportTextures)
+    @classmethod
+    def run(cls):
         bpy.ops.import_scene.custom_textures('INVOKE_DEFAULT')
 
 
@@ -195,17 +310,24 @@ class ImportFbx(Operator, ImportHelper):
     
     @classmethod
     def run(cls):
-        # Register and call
-        bpy.utils.register_class(ImportFbx)
         bpy.ops.import_scene.custom_fbx('INVOKE_DEFAULT')
 
 
-ImportFbx.run()
-#ImportTextures.run()
+
+def register():
+    bpy.utils.register_class(ImportFbx)
+    bpy.utils.register_class(ImportTextures)
+
+def unregister():
+    bpy.utils.unregister_class(ImportFbx)
+    bpy.utils.register_class(ImportTextures)
+
+# Add the typical block to avoid execution on import
+if __name__ == "__main__":
+    register()
 
 #mapping_node = nodes.new(type="ShaderNodeMapping")
 #texcoord_node = nodes.new(type="ShaderNodeTexCoord")
 #node_dict.update({"MappingNode":mapping_node, "TexCoordNode":texcoord_node})
 
 print("Goodbye")
-print("My Script Finished: %.4f sec" % (time.time() - time_start))
---
+++ main.py
@@ -0,0 +1,17 @@
+import bpy
+import os
+import sys
+import importlib
+
+script_dir = os.path.dirname(bpy.context.space_data.text.filepath)
+sys.path.append(script_dir)
+
+from Megascans import ImportMegascans as Mega
+importlib.reload(Mega)
+Mega.register()
+
+fbx_import = Mega.ImportFbx
+#fbx_import.run()
+
+texture_import = Mega.ImportTextures
+texture_import.run()
